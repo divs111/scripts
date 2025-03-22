d2cb6b13258a1b480a4ba3031b1f6345aae50290
divs111 <divyasoni01@icloud.com>
2025-03-22 13:47:42 +0000

    done

--- backup.sh
+++ backup.sh
@@ -15,4 +15,4 @@ timestamp=$(date '+%Y-%m-%d-%H-%M')
 zip -r "$dest/backup-$timestamp.zip" "$src" > /dev/null
 
 aws s3 sync $dest s3://div1-bucket 
-echo "Backup completed"
+echo "Backup completed successfully"
